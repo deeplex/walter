22faf73c487d86e2de6e1fd053b3d6662cf5e575
Kai Lawrence <mail@kailawrence.de>
2020-05-11 16:24:50 +0200

    Replace VertragGridControl with specialized Grids in respective Pages.

--- Deeplex.Saverwalter.App/ViewModels/KontaktDetailViewModel.cs
+++ Deeplex.Saverwalter.App/ViewModels/KontaktDetailViewModel.cs
@@ -1,5 +1,9 @@
 ﻿using Deeplex.Saverwalter.Model;
 using Deeplex.Utils.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Deeplex.Saverwalter.App.ViewModels
 {
@@ -13,6 +17,8 @@ namespace Deeplex.Saverwalter.App.ViewModels
         public ObservableProperty<string> Telefon { get; } = new ObservableProperty<string>();
         public ObservableProperty<string> Mobil { get; } = new ObservableProperty<string>();
         public ObservableProperty<string> Fax { get; } = new ObservableProperty<string>();
+        public ObservableProperty<List<KontaktDetailVertrag>> Vertraege
+            = new ObservableProperty<List<KontaktDetailVertrag>>();
 
         public string Name => Vorname.Value + " " + Nachname.Value;
 
@@ -31,6 +37,14 @@ namespace Deeplex.Saverwalter.App.ViewModels
             Fax.Value = k.Fax ?? "";
             Telefon.Value = k.Telefon ?? "";
             Mobil.Value = k.Mobil ?? "";
+
+            Vertraege.Value = App.Walter.Vertraege
+                .Include(v => v.Mieter)
+                .ThenInclude(m => m.Kontakt)
+                .Include(v => v.Wohnung).ToList()
+                .Where(v => v.Mieter.Exists(m => m.KontaktId == Id))
+                .Select(v => new KontaktDetailVertrag(v.VertragId))
+                .ToList();
 
             BeginEdit = new RelayCommand(_ => IsInEdit.Value = true, _ => !IsInEdit.Value);
             IsInEdit.PropertyChanged += (_, ev) => BeginEdit.RaiseCanExecuteChanged(ev);
@@ -60,5 +74,46 @@ namespace Deeplex.Saverwalter.App.ViewModels
 
         public RelayCommand BeginEdit { get; }
         public RelayCommand SaveEdit { get; }
+
+
+        public class KontaktDetailVertrag
+        {
+            public int Id { get; }
+            public int Version { get; }
+            public ObservableProperty<string> Anschrift { get; } = new ObservableProperty<string>();
+            public ObservableProperty<string> Wohnung { get; } = new ObservableProperty<string>();
+            public ObservableProperty<DateTime> Beginn { get; } = new ObservableProperty<DateTime>();
+            public ObservableProperty<string> AuflistungMieter { get; } = new ObservableProperty<string>();
+            public ObservableProperty<string> BeginnString { get; } = new ObservableProperty<string>();
+            public ObservableProperty<string> EndeString { get; } = new ObservableProperty<string>();
+            public ObservableProperty<List<VertragVersionListViewModel>> Versionen { get; }
+                = new ObservableProperty<List<VertragVersionListViewModel>>();
+
+            public KontaktDetailVertrag(Guid id)
+                : this(App.Walter.Vertraege.Where(v => v.VertragId == id)) { }
+
+            private KontaktDetailVertrag(IEnumerable<Vertrag> v)
+                : this(v.OrderBy(vs => vs.Version).Last())
+            {
+                Versionen.Value = v.OrderBy(vs => vs.Version).Select(vs => new VertragVersionListViewModel(vs)).ToList();
+                BeginnString.Value = Versionen.Value.First().BeginnString.Value;
+                Beginn.Value = Versionen.Value.First().Beginn.Value;
+            }
+
+            private KontaktDetailVertrag(Vertrag v)
+            {
+                Id = v.rowid;
+                Version = v.Version;
+                Anschrift.Value = v.Wohnung is Wohnung w ? Utils.Anschrift(w) : "";
+                Wohnung.Value = v.Wohnung is Wohnung ww ? ww.Bezeichnung : "";
+
+                Beginn.Value = v.Beginn;
+                BeginnString.Value = v.Beginn.ToShortDateString(); ;
+                EndeString.Value = v.Ende is DateTime e ? e.ToShortDateString() : "";
+
+                AuflistungMieter.Value = string.Join(", ", v.Mieter.Select(m => m.Kontakt.Nachname));
+            }
+        }
+
     }
 }
--- Deeplex.Saverwalter.App/ViewModels/WohnungDetailViewModel.cs
+++ Deeplex.Saverwalter.App/ViewModels/WohnungDetailViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using Deeplex.Saverwalter.Model;
 using Deeplex.Utils.ObjectModel;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -14,9 +15,10 @@ namespace Deeplex.Saverwalter.App.ViewModels
         public ObservableProperty<string> Anschrift = new ObservableProperty<string>();
         public ObservableProperty<double> Wohnflaeche = new ObservableProperty<double>();
         public ObservableProperty<double> Nutzflaeche = new ObservableProperty<double>();
-
         public ObservableProperty<List<WohnungDetailZaehler>> Zaehler
             = new ObservableProperty<List<WohnungDetailZaehler>>();
+        public ObservableProperty<List<WohnungDetailVertrag>> Vertraege
+            = new ObservableProperty<List<WohnungDetailVertrag>>();
 
         public WohnungDetailViewModel(int id)
             : this(App.Walter.Wohnungen.Include(w => w.Zaehler).First(w => w.WohnungId == id)) { }
@@ -31,6 +33,14 @@ namespace Deeplex.Saverwalter.App.ViewModels
             Nutzflaeche.Value = w.Nutzflaeche;
 
             Zaehler.Value = w.Zaehler.Select(z => new WohnungDetailZaehler(z)).ToList();
+
+            Vertraege.Value = App.Walter.Vertraege
+                .Include(v => v.Mieter)
+                .ThenInclude(m => m.Kontakt)
+                .Include(v => v.Wohnung).ToList()
+                .Where(v => v.Wohnung.WohnungId == Id)
+                .Select(v => new WohnungDetailVertrag(v.VertragId))
+                .ToList();
         }
     }
 
@@ -45,6 +55,42 @@ namespace Deeplex.Saverwalter.App.ViewModels
             Id = z.ZaehlerId;
             Kennnummer.Value = "312k2112nj1fnj21"; // TODO Add Zaehler.Kennummer to Model.cs
             Typ.Value = z.Typ.ToString(); // May be a descript thingy later on?...
+        }
+    }
+
+    public class WohnungDetailVertrag
+    {
+        public int Id { get; }
+        public int Version { get; }
+        public ObservableProperty<DateTime> Beginn { get; } = new ObservableProperty<DateTime>();
+        public ObservableProperty<string> AuflistungMieter { get; } = new ObservableProperty<string>();
+        public ObservableProperty<string> BeginnString { get; } = new ObservableProperty<string>();
+        public ObservableProperty<string> EndeString { get; } = new ObservableProperty<string>();
+        public ObservableProperty<List<VertragVersionListViewModel>> Versionen { get; }
+            = new ObservableProperty<List<VertragVersionListViewModel>>();
+
+        public WohnungDetailVertrag(Guid id)
+            : this(App.Walter.Vertraege.Where(v => v.VertragId == id)) { }
+
+        private WohnungDetailVertrag(IEnumerable<Vertrag> v)
+            : this(v.OrderBy(vs => vs.Version).Last())
+        {
+            Versionen.Value = v.OrderBy(vs => vs.Version).Select(vs => new VertragVersionListViewModel(vs)).ToList();
+            BeginnString.Value = Versionen.Value.First().BeginnString.Value;
+            Beginn.Value = Versionen.Value.First().Beginn.Value;
+        }
+
+        private WohnungDetailVertrag(Vertrag v)
+        {
+            Id = v.rowid;
+            Version = v.Version;
+
+            Beginn.Value = v.Beginn;
+            BeginnString.Value = v.Beginn.ToShortDateString(); ;
+            EndeString.Value = v.Ende is DateTime e ? e.ToShortDateString() : "";
+
+            AuflistungMieter.Value = string.Join(", ", v.Mieter.Select(m =>
+                (m.Kontakt.Vorname is string n ? n + " " : "") + m.Kontakt.Nachname));
         }
     }
 }
