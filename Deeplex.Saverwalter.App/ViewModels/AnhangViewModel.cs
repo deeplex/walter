2488b116721ec19cb62c12de22b7e0642a5aa076
Kai Lawrence <mail@kailawrence.de>
2020-06-21 22:06:38 +0200

    Move Anhang in designated file.

---
+++ Deeplex.Saverwalter.App/ViewModels/AnhangViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Deeplex.Saverwalter.Model;
+using Deeplex.Utils.ObjectModel;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Immutable;
+using System.Linq;
+
+namespace Deeplex.Saverwalter.App.ViewModels
+{
+    public sealed class AnhangViewModel : BindableBase
+    {
+        public ObservableProperty<ImmutableList<AnhangDatei>> Dateien = new ObservableProperty<ImmutableList<AnhangDatei>>();
+
+        public AnhangViewModel()
+        {
+            Dateien.Value = App.Walter.Anhaenge.Select(a => new AnhangDatei(a)).ToImmutableList();
+        }
+
+    }
+
+    public sealed class AnhangDatei : BindableBase
+    {
+        public Anhang Entity { get; }
+
+        public AnhangDatei(Anhang a)
+        {
+            Entity = a;
+        }
+
+        public string DateiName => Entity.FileName;
+    }
+}
--- Deeplex.Saverwalter.App/ViewModels/MainViewModel.cs
+++ Deeplex.Saverwalter.App/ViewModels/MainViewModel.cs
@@ -7,22 +7,9 @@ namespace Deeplex.Saverwalter.App.ViewModels
 {
     public sealed class MainViewModel
     {
-        public ObservableProperty<ImmutableList<AnhangViewModel>> Anhang = new ObservableProperty<ImmutableList<AnhangViewModel>>();
+        public AnhangViewModel Anhang = new AnhangViewModel();
         public MainViewModel()
         {
-            Anhang.Value = App.Walter.Anhaenge.Select(a => new AnhangViewModel(a)).ToImmutableList();
         }
-    }
-
-    public sealed class AnhangViewModel : BindableBase
-    {
-        public Anhang Entity { get; }
-
-        public AnhangViewModel(Anhang a)
-        {
-            Entity = a;
-        }
-
-        public string DateiName => Entity.FileName;
     }
 }
