3199e00dc8519a02b529bf0a3714f21aa958604d
Kai Lawrence <mail@kailawrence.de>
2020-06-23 15:12:48 +0200

    Implement prototypical scaffolding for explorer filters.

--- Deeplex.Saverwalter.App/ViewModels/AnhangViewModel.cs
+++ Deeplex.Saverwalter.App/ViewModels/AnhangViewModel.cs
@@ -9,12 +9,29 @@ namespace Deeplex.Saverwalter.App.ViewModels
     public sealed class AnhangViewModel : BindableBase
     {
         public ObservableProperty<ImmutableList<AnhangDatei>> Dateien = new ObservableProperty<ImmutableList<AnhangDatei>>();
+        public ObservableProperty<string>FilterText = new ObservableProperty<string>();
+
+        public ObservableProperty<ImmutableList<AnhangKontakt>> AnhangKontaktList = new ObservableProperty<ImmutableList<AnhangKontakt>>();
+        public ObservableProperty<ImmutableList<AnhangVertrag>> AnhangVertragList = new ObservableProperty<ImmutableList<AnhangVertrag>>();
+        public ObservableProperty<ImmutableList<AnhangAdresse>> AnhangAdresseList = new ObservableProperty<ImmutableList<AnhangAdresse>>();
 
         public AnhangViewModel()
         {
             Dateien.Value = App.Walter.Anhaenge.Select(a => new AnhangDatei(a)).ToImmutableList();
-        }
+            FilterText.Value = "Hier könnte ein sinnvoller Filtertext stehen...";
 
+            AnhangKontaktList.Value = App.Walter.JuristischePersonen.ToImmutableList()
+                .Select(j => new AnhangKontakt(j))
+                .Concat(App.Walter.NatuerlichePersonen.Select(n => new AnhangKontakt(n)))
+                .ToImmutableList();
+
+            AnhangVertragList.Value = App.Walter.Vertraege
+                .Include(v => v.Wohnung)
+                .ThenInclude(w => w.Adresse)
+                .Select(v => new AnhangVertrag(v)).ToImmutableList();
+
+            AnhangAdresseList.Value = App.Walter.Adressen.Select(a => new AnhangAdresse(a)).ToImmutableList();
+        }
     }
 
     public sealed class AnhangDatei : BindableBase
@@ -27,5 +44,48 @@ namespace Deeplex.Saverwalter.App.ViewModels
         }
 
         public string DateiName => Entity.FileName;
+    }
+    public sealed class AnhangKontakt
+    {
+        public string Bezeichnung { get; }
+        public ObservableProperty<ImmutableList<AnhangVertrag>> Vertraege = new ObservableProperty<ImmutableList<AnhangVertrag>>();
+
+        public AnhangKontakt(IPerson p)
+        {
+            Bezeichnung = p.Bezeichnung;
+
+            var ms = App.Walter.MieterSet.ToList();
+
+            Vertraege.Value = App.Walter.Vertraege
+                .Include(v => v.Wohnung).ThenInclude(w => w.Adresse)
+                .ToList()
+                .Where(v => v.Wohnung.BesitzerId == p.PersonId || ms.Exists(m => m.VertragId == v.VertragId && m.PersonId == p.PersonId))
+                .Select(v => new AnhangVertrag(v))
+                .ToImmutableList();
+        }
+    }
+
+    public sealed class AnhangVertrag
+    {
+        public string Bezeichnung { get; }
+
+        public AnhangVertrag(Vertrag v)
+        {
+            var bs = App.Walter.MieterSet.Where(m => m.VertragId == v.VertragId).ToList();
+            var cs = bs.Select(b => App.Walter.FindPerson(b.PersonId).Bezeichnung);
+            var mieter = string.Join(", ", cs);
+
+            Bezeichnung = mieter + " – " + v.Wohnung.Adresse.Strasse + " " + v.Wohnung.Adresse.Hausnummer + " – " + v.Wohnung.Bezeichnung;
+        }
+    }
+
+    public sealed class AnhangAdresse
+    {
+        public string Bezeichnung { get; }
+
+        public AnhangAdresse(Adresse a)
+        {
+            Bezeichnung = AdresseViewModel.Anschrift(a);
+        }
     }
 }
