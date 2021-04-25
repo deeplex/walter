ad5975d3e8c0fca29f25bb24d84d6634caf0f480
Kai Lawrence <mail@kailawrence.de>
2021-04-25 16:18:15 +0200

    Use KontaktListPage as UserControl

---
+++ Deeplex.Saverwalter.App/UserControls/KontaktListControl.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Deeplex.Saverwalter.App.ViewModels;
+using Deeplex.Saverwalter.App.Views;
+using Deeplex.Saverwalter.Model;
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Deeplex.Saverwalter.App.UserControls
+{
+    public sealed partial class KontaktListControl : UserControl
+    {
+        public KontaktListViewModel ViewModel { get; set; }
+
+        public KontaktListControl()
+        {
+            InitializeComponent();
+            ViewModel = new KontaktListViewModel();
+        }
+
+        private void Details_Click(object sender, RoutedEventArgs e)
+        {
+            var sk = ViewModel.SelectedKontakt;
+            if (sk != null)
+            {
+                var target =
+                    sk.Type == typeof(NatuerlichePerson) ? typeof(NatuerlichePersonDetailPage) :
+                    sk.Type == typeof(JuristischePerson) ? typeof(JuristischePersonenDetailPage) :
+                    null;
+
+                App.ViewModel.Navigate(target, App.Walter.FindPerson(sk.Guid));
+            }
+        }
+
+
+
+        private void DataGrid_RightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
+        {
+            ViewModel.SelectedKontakt = (e.OriginalSource as FrameworkElement).DataContext as KontaktListEntry;
+        }
+
+        // Using a DependencyProperty as the backing store for Property1.  
+        // This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty WohnungIdProperty
+            = DependencyProperty.Register(
+                  "WohnungId",
+                  typeof(int),
+                  typeof(VertragListControl),
+                  new PropertyMetadata(0));
+    }
+}
--- Deeplex.Saverwalter.App/Views/Personen/KontaktListPage.xaml.cs
+++ Deeplex.Saverwalter.App/Views/Personen/KontaktListPage.xaml.cs
@@ -55,20 +55,6 @@ namespace Deeplex.Saverwalter.App.Views
             return Button;
         }
 
-        private void Details_Click(object sender, RoutedEventArgs e)
-        {
-            var sk = ViewModel.SelectedKontakt;
-            if (sk != null)
-            {
-                var target =
-                    sk.Type == typeof(NatuerlichePerson) ? typeof(NatuerlichePersonDetailPage) :
-                    sk.Type == typeof(JuristischePerson) ? typeof(JuristischePersonenDetailPage) :
-                    null;
-
-                App.ViewModel.Navigate(target, App.Walter.FindPerson(sk.Guid));
-            }
-        }
-
         private void AddNatuerlichePerson_Click(object sender, RoutedEventArgs e)
         {
             Frame.Navigate(typeof(NatuerlichePersonDetailPage), null,
@@ -79,11 +65,6 @@ namespace Deeplex.Saverwalter.App.Views
         {
             Frame.Navigate(typeof(JuristischePersonenDetailPage), null,
                 new DrillInNavigationTransitionInfo());
-        }
-
-        private void DataGrid_RightTapped(object sender, Windows.UI.Xaml.Input.RightTappedRoutedEventArgs e)
-        {
-            ViewModel.SelectedKontakt = (e.OriginalSource as FrameworkElement).DataContext as KontaktListEntry;
         }
     }
 }
