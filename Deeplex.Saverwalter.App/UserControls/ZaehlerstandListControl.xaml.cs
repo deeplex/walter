861aca4b08f1da8c8dca8d110aff2e39db19f666
Kai Lawrence <mail@kailawrence.de>
2021-04-26 13:46:01 +0200

    Implement ZaehlerstandListControl

---
+++ Deeplex.Saverwalter.App/UserControls/ZaehlerstandListControl.xaml.cs
@@ -0,0 +1,47 @@
+﻿using Deeplex.Saverwalter.App.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Deeplex.Saverwalter.App.UserControls
+{
+    public sealed partial class ZaehlerstandListControl : UserControl
+    {
+        public ZaehlerstandListViewModel ViewModel { get; set; }
+
+        public ZaehlerstandListControl()
+        {
+            InitializeComponent();
+            RegisterPropertyChangedCallback(ZaehlerIdProperty, (ZaehlerIdDepObject, ZaehlerIdProp) =>
+            {
+                ViewModel = new ZaehlerstandListViewModel(App.Walter.ZaehlerSet.Find(ZaehlerId));
+            });
+        }
+
+        public int ZaehlerId
+        {
+            get { return (int)GetValue(ZaehlerIdProperty); }
+            set { SetValue(ZaehlerIdProperty, value); }
+        }
+
+        public static readonly DependencyProperty ZaehlerIdProperty
+            = DependencyProperty.Register(
+            "ZaehlerId",
+            typeof(int),
+            typeof(ZaehlerstandListControl),
+            new PropertyMetadata(0));
+    }
+}
---
+++ Deeplex.Saverwalter.App/ViewModels/Zähler/ZaehlerstandListViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Deeplex.Saverwalter.Model;
+using Deeplex.Utils.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deeplex.Saverwalter.App.ViewModels
+{
+    public class ZaehlerstandListViewModel
+    {
+        public ObservableProperty<List<ZaehlerstandListEntry>> Liste = new ObservableProperty<List<ZaehlerstandListEntry>>();
+
+        public ZaehlerstandListViewModel(Zaehler z)
+        {
+            Liste.Value = z.Staende.Select(s => new ZaehlerstandListEntry(s)).ToList();
+        }
+
+        public class ZaehlerstandListEntry
+        {
+            private Zaehlerstand Entity;
+            public int Id => Entity.ZaehlerstandId;
+            public double Stand => Entity.Stand;
+            public string StandString => Entity.Stand.ToString();
+            public DateTimeOffset Datum => Entity.Datum;
+            public string DatumString => Datum.ToString("dd.MM.yyyy");
+
+            public ZaehlerstandListEntry(Zaehlerstand z)
+            {
+                Entity = z;
+            }
+        }
+    }
+}
