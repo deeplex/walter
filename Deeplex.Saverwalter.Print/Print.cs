cd810daa2714b1d7d67294eec1598adb52066eb6
Kai Lawrence <mail@kailawrence.de>
2021-04-28 22:26:02 +0200

    Fix Anschrift Vermieter

--- Deeplex.Saverwalter.Print/Print.cs
+++ Deeplex.Saverwalter.Print/Print.cs
@@ -3,6 +3,7 @@ using DocumentFormat.OpenXml;
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using static Deeplex.Saverwalter.Model.Betriebskostenabrechnung;
 
@@ -38,7 +39,7 @@ namespace Deeplex.Saverwalter.Print
             body.Append(
                 // p.1
                 AnschriftVermieter(b),
-                EmptyRows(4),
+                EmptyRows(3),
                 PostalischerVermerk(b),
                 PrintDate(),
                 Betreff(b),
@@ -122,22 +123,42 @@ namespace Deeplex.Saverwalter.Print
 
             var ap = b.Ansprechpartner;
 
-            // TODO Ansprechpartner HAS to have a Adresse...
-            var table = new Table(
-                new TableWidth() { Width = "5000", Type = TableWidthUnitValues.Pct },
-                new TableRow(
-                    ContentCell(b.Vermieter.Bezeichnung),
-                    ContentCell("", JustificationValues.Right)),
-                new TableRow(
-                    ContentCell("℅ " + AnsprechpartnerBezeichnung),
-                    ContentCell(ap.Telefon != "" ? "Tel.: " + ap.Telefon : "", JustificationValues.Right)),
-                new TableRow(
-                    ContentCell(b.Ansprechpartner.Adresse!.Strasse + " " + b.Ansprechpartner.Adresse.Hausnummer),
-                    ContentCell(ap.Email != "" ? "E-Mail: " + ap.Email : "", JustificationValues.Right)));
-            new TableRow(
-                    ContentCell(b.Ansprechpartner.Adresse.Postleitzahl + " " + b.Ansprechpartner.Adresse.Stadt),
-                    ContentCell(ap.Fax != "" ? "Fax: " + ap.Fax : "", JustificationValues.Right));
+            var left = new List<TableCell>();
+            var right = new List<TableCell>();
 
+            left.Add(ContentCell(b.Vermieter.Bezeichnung));
+            if (b.Vermieter.Bezeichnung != AnsprechpartnerBezeichnung)
+            {
+                left.Add(ContentCell("℅ " + AnsprechpartnerBezeichnung));
+            }
+            left.Add(ContentCell(b.Ansprechpartner.Adresse!.Strasse + " " + b.Ansprechpartner.Adresse.Hausnummer));
+            left.Add(ContentCell(b.Ansprechpartner.Adresse!.Postleitzahl + " " + b.Ansprechpartner.Adresse.Stadt));
+
+            right.Add(ContentCell("", JustificationValues.Right));
+            if (ap.Telefon != null && ap.Telefon != "")
+            {
+                right.Add(ContentCell("Tel.: " + ap.Telefon, JustificationValues.Right));
+            }
+            if (ap.Fax != null && ap.Fax != "")
+            {
+                right.Add(ContentCell("Fax: " + ap.Fax, JustificationValues.Right));
+            }
+            if (ap.Email != null && ap.Email != "")
+            {
+                right.Add(ContentCell("E-Mail: " + ap.Email, JustificationValues.Right));
+            }
+
+            // TODO Ansprechpartner HAS to have a Adresse...
+            var table = new Table(new TableWidth() { Width = "5000", Type = TableWidthUnitValues.Pct });
+
+            for (var i = 0; i < Math.Max(left.Count, right.Count); i++)
+            {
+                var row = new TableRow();
+                
+                if (left.Count > i) row.Append(left[i]);
+                if (right.Count > i) row.Append(right[i]);
+                table.Append(row);
+            }
 
             return table;
         }
