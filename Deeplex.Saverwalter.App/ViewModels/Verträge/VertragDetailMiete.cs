e420778ac5ca51b1f03181be34885622f068a12f
Kai Lawrence <mail@kailawrence.de>
2021-04-29 19:07:33 +0200

    Refactor VertragDetail Classes into separate files.

---
+++ Deeplex.Saverwalter.App/ViewModels/Verträge/VertragDetailMiete.cs
@@ -0,0 +1,117 @@
+﻿using Deeplex.Saverwalter.Model;
+using Deeplex.Utils.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deeplex.Saverwalter.App.ViewModels
+{
+    public sealed class VertragDetailMiete : BindableBase
+    {
+        public void selfDestruct()
+        {
+            App.Walter.Remove(Entity);
+            App.SaveWalter();
+        }
+        private Miete Entity { get; }
+
+        public DateTimeOffset Zahlungsdatum
+        {
+            get => Entity.Zahlungsdatum.AsUtcKind();
+            set
+            {
+                var old = Entity.Zahlungsdatum;
+                Entity.Zahlungsdatum = value.UtcDateTime;
+                RaisePropertyChangedAuto(old, value.UtcDateTime);
+            }
+        }
+
+        public DateTimeOffset BetreffenderMonat
+        {
+            get => Entity.BetreffenderMonat.AsUtcKind();
+            set
+            {
+                var old = Entity.BetreffenderMonat;
+                Entity.Zahlungsdatum = new DateTime(value.Year, value.Month, 1).AsUtcKind();
+                RaisePropertyChangedAuto(old, Entity.Zahlungsdatum);
+            }
+        }
+
+        public string Notiz
+        {
+            get => Entity.Notiz;
+            set
+            {
+                var old = Entity.Notiz;
+                Entity.Notiz = value;
+                RaisePropertyChangedAuto(old, value);
+            }
+        }
+
+        public double Betrag
+        {
+            get => Entity.Betrag ?? 0;
+            set
+            {
+                var old = Entity.Betrag;
+                Entity.Betrag = value;
+                RaisePropertyChangedAuto(old, value);
+            }
+        }
+
+        public VertragDetailMiete(Guid vertragId)
+            : this(new Miete
+            {
+                VertragId = vertragId,
+                Zahlungsdatum = DateTime.UtcNow.Date,
+            })
+        {
+        }
+
+        public VertragDetailMiete(Miete m)
+        {
+            Entity = m;
+            PropertyChanged += OnUpdate;
+            AttachFile = new AsyncRelayCommand(async _ =>
+                await Utils.Files.SaveFilesToWalter(App.Walter.MieteAnhaenge, m), _ => true);
+        }
+
+        public AsyncRelayCommand AttachFile;
+
+        private bool savable =>
+            Entity.Betrag != null &&
+            Entity.BetreffenderMonat != null &&
+            Entity.Zahlungsdatum != null &&
+            Entity.VertragId != null;
+
+        private void OnUpdate(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(Betrag):
+                case nameof(Zahlungsdatum):
+                case nameof(Notiz):
+                case nameof(BetreffenderMonat):
+                    break;
+                default:
+                    return;
+            }
+
+            if (!savable) return;
+
+            if (Entity.MieteId != 0)
+            {
+                App.Walter.Mieten.Update(Entity);
+            }
+            else
+            {
+                App.Walter.Mieten.Add(Entity);
+            }
+            App.SaveWalter();
+        }
+    }
+
+}
---
+++ Deeplex.Saverwalter.App/ViewModels/Verträge/VertragDetailVersion.cs
@@ -0,0 +1,155 @@
+﻿using Deeplex.Saverwalter.Model;
+using Deeplex.Utils.ObjectModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deeplex.Saverwalter.App.ViewModels
+{
+    public class VertragDetailVersion : BindableBase
+    {
+        private Vertrag Entity { get; }
+        public int Id => Entity.rowid;
+        public int Version => Entity.Version;
+        public double KaltMiete
+        {
+            get => Entity.KaltMiete;
+            set
+            {
+                var old = Entity.KaltMiete;
+                Entity.KaltMiete = value;
+                RaisePropertyChangedAuto(old, value);
+            }
+        }
+
+        public int Personenzahl
+        {
+            get => Entity.Personenzahl;
+            set
+            {
+                var old = Entity.Personenzahl;
+                Entity.Personenzahl = value;
+                RaisePropertyChangedAuto(old, value);
+            }
+        }
+
+        private VertragDetailWohnung mWohnung;
+        public VertragDetailWohnung Wohnung
+        {
+            get => mWohnung;
+            set
+            {
+                if (value == null) return;
+                mWohnung = value;
+                Entity.WohnungId = mWohnung.Id;
+                var old = mWohnung.Id;
+                if (RaisePropertyChangedAuto(old, value.Id))
+                {
+                    RaisePropertyChanged(nameof(Vermieter));
+                }
+            }
+        }
+        public DateTimeOffset Beginn
+        {
+            get => Entity.Beginn.AsUtcKind();
+            set
+            {
+                var old = Entity.Beginn;
+                Entity.Beginn = value.UtcDateTime;
+                RaisePropertyChangedAuto(old, value);
+            }
+        }
+
+        public DateTimeOffset? Ende
+        {
+            get => Entity.Ende?.AsUtcKind();
+            set
+            {
+                var old = Entity.Ende;
+                Entity.Ende = value?.UtcDateTime;
+                RaisePropertyChangedAuto(old, value?.UtcDateTime);
+            }
+        }
+
+        public string Notiz
+        {
+            get => Entity.Notiz;
+            set
+            {
+                var old = Entity.Notiz;
+                Entity.Notiz = value;
+                RaisePropertyChangedAuto(old, value);
+            }
+        }
+
+        public string Vermieter => Wohnung.Besitzer;
+        private VertragDetailKontakt mAnsprechpartner;
+        public VertragDetailKontakt Ansprechpartner
+        {
+            get => mAnsprechpartner;
+            set
+            {
+                mAnsprechpartner = value;
+                Entity.AnsprechpartnerId = mAnsprechpartner.PersonId;
+                RaisePropertyChangedAuto();
+            }
+        }
+
+        public VertragDetailVersion(int id) : this(App.Walter.Vertraege.Find(id)) { }
+        public VertragDetailVersion(Vertrag v)
+        {
+            Entity = v;
+
+            if (v.AnsprechpartnerId != Guid.Empty && v.AnsprechpartnerId != null)
+            {
+                Ansprechpartner = new VertragDetailKontakt(v.AnsprechpartnerId.Value);
+            }
+
+            if (v.Wohnung != null)
+            {
+                Wohnung = new VertragDetailWohnung(v.Wohnung);
+            }
+            PropertyChanged += OnUpdate;
+        }
+
+        private void OnUpdate(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case nameof(Wohnung):
+                case nameof(Beginn):
+                case nameof(Ende):
+                case nameof(Notiz):
+                case nameof(Personenzahl):
+                case nameof(KaltMiete):
+                // case nameof(VersionsNotiz):
+                case nameof(Ansprechpartner):
+                    break;
+                default:
+                    return;
+            }
+
+            if (Entity.VertragId == null ||
+                Entity.Beginn == null ||
+                (Entity.Wohnung == null && Entity.WohnungId == 0) ||
+                Entity.AnsprechpartnerId == null)
+            {
+                return;
+            }
+
+            if (Entity.rowid != 0)
+            {
+                App.Walter.Vertraege.Update(Entity);
+            }
+            else
+            {
+                App.Walter.Vertraege.Add(Entity);
+            }
+            App.SaveWalter();
+        }
+    }
+
+}
