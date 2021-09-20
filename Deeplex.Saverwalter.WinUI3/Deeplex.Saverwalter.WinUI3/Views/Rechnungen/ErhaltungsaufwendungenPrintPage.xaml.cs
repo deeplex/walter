6b8cab8fd97d348c1a35e6bb08cf68758ec70d9f
Kai Lawrence <mail@kailawrence.de>
2021-09-20 20:26:11 +0200

    WIP - Add ErhaltungsaufwendungenPrintPage

---
+++ Deeplex.Saverwalter.WinUI3/Deeplex.Saverwalter.WinUI3/Views/Rechnungen/ErhaltungsaufwendungenPrintPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Deeplex.Saverwalter.Model;
+using Deeplex.Saverwalter.ViewModels;
+using Deeplex.Saverwalter.ViewModels.Rechnungen;
+using Deeplex.Saverwalter.WinUI3.UserControls;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Navigation;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Deeplex.Saverwalter.WinUI3.Views.Rechnungen
+{
+    public sealed partial class ErhaltungsaufwendungenPrintPage : Page
+    {
+        public List<ErhaltungsaufwendungenListViewModel> ViewModel { get; set; }
+
+        public ErhaltungsaufwendungenPrintPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if (e.Parameter is List<ErhaltungsaufwendungenListViewModel> vm)
+            {
+                ViewModel = vm;
+            }
+            
+            //App.Window.CommandBar.MainContent = new ErhaltungsaufwendungenPrintCommandBarControl { ViewModel = ViewModel };
+
+            base.OnNavigatedTo(e);
+        }
+    }
+}
