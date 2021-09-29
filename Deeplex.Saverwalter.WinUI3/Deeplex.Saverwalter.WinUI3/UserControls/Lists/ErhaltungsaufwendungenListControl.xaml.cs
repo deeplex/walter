f7c0f85e46f74ab394b6c24d684ccd654aff4f92
klawr <mail@kailawrence.de>
2021-09-29 14:44:43 +0200

    Move converter into separate Utils file

--- Deeplex.Saverwalter.WinUI3/Deeplex.Saverwalter.WinUI3/UserControls/Lists/ErhaltungsaufwendungenListControl.xaml.cs
+++ Deeplex.Saverwalter.WinUI3/Deeplex.Saverwalter.WinUI3/UserControls/Lists/ErhaltungsaufwendungenListControl.xaml.cs
@@ -121,25 +121,4 @@ namespace Deeplex.Saverwalter.WinUI3.UserControls
             ViewModel.Liste.Value = (sender as DataGrid).Sort(e.Column, ViewModel.Liste.Value);
         }
     }
-
-    public class IsEnabledConverter : IValueConverter
-    {
-        public SolidColorBrush enabled => Application.Current.Resources["enabled"] as SolidColorBrush;
-        public SolidColorBrush disabled => Application.Current.Resources["disabled"] as SolidColorBrush;
-
-        public object Convert(object value, Type targetType, object parameter, string language)
-        {
-            bool? isEnabled = (bool)value;
-            if (isEnabled.HasValue && isEnabled.Value == true)
-            {
-                return enabled;
-            }
-            return disabled;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, string language)
-        {
-            return enabled;
-        }
-    }
 }
---
+++ Deeplex.Saverwalter.WinUI3/Deeplex.Saverwalter.WinUI3/Utils/Converter.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Media;
+using System;
+
+namespace Deeplex.Saverwalter.WinUI3.Utils
+{
+    public class IsEnabledConverter : IValueConverter
+    {
+        public SolidColorBrush enabled => Application.Current.Resources["enabled"] as SolidColorBrush;
+        public SolidColorBrush disabled => Application.Current.Resources["disabled"] as SolidColorBrush;
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool? isEnabled = (bool)value;
+            if (isEnabled.HasValue && isEnabled.Value == true)
+            {
+                return enabled;
+            }
+            return disabled;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            return enabled;
+        }
+    }
+}
