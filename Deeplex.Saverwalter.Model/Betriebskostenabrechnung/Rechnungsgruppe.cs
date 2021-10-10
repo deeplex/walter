1009b5d8b1a30a2aef836d92afd9f89994ac4ee0
Kai Lawrence <mail@kailawrence.de>
2021-10-10 22:08:10 +0200

    Implement Tage in BetriebskostenrechnungPrintPage

--- Deeplex.Saverwalter.Model/Betriebskostenabrechnung/Rechnungsgruppe.cs
+++ Deeplex.Saverwalter.Model/Betriebskostenabrechnung/Rechnungsgruppe.cs
@@ -118,6 +118,8 @@ namespace Deeplex.Saverwalter.Model
     {
         public DateTime Beginn { get; }
         public DateTime Ende { get; }
+        public int Tage => (Ende - Beginn).Days + 1;
+        public int GesamtTage => (new DateTime(Ende.Year, 12, 31) - new DateTime(Ende.Year, 1, 1)).Days + 1;
         public int Personenzahl { get; }
         public Rechnungsgruppe Parent { get; }
 
