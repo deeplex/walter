bd91c696e6bc1711da34c24e4d2017f59b78e412
klawr <mail@kailawrence.de>
2022-04-24 21:42:20 +0200

    Remove deprecated AnhangService
    Was never used...

---
+++ Deeplex.Saverwalter.ServicesTests/WalterDbServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+using Deeplex.Saverwalter.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Deeplex.Saverwalter.Services.Tests
+{
+    public class WalterDbServiceTests
+    {
+        [Fact()]
+        public void WalterDbServiceTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void SaveWalterTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
