ea1c7e8f92c4bcd018287531979870a7727c8675
klawr <mail@kailawrence.de>
2022-04-24 23:27:32 +0200

    Add BetriebskostenabrechnungTest

--- Deeplex.Saverwalter.ModelTests/Betriebskostenabrechnung/BetriebskostenabrechnungTests.cs
+++ Deeplex.Saverwalter.ModelTests/Betriebskostenabrechnung/BetriebskostenabrechnungTests.cs
@@ -1,13 +1,26 @@
-﻿using Xunit;
+﻿using FakeItEasy;
+using FluentAssertions;
+using System;
+using Xunit;
 
 namespace Deeplex.Saverwalter.Model.Tests
 {
     public class BetriebskostenabrechnungTests
     {
-        [Fact()]
-        public void BetriebskostenabrechnungTest()
+        [Theory]
+        [InlineData(2020)]
+        public void BetriebskostenabrechnungTest(int jahr)
         {
-            Assert.True(false, "This test needs an implementation");
+            var vfake = A.Fake<Vertrag>();
+            var dfake = A.Fake<SaverwalterContext>();
+
+            var beginn = DateTime.Parse("01/01/" + jahr.ToString());
+            var ende = DateTime.Parse("12/31/" + jahr.ToString());
+
+            var stub = new Betriebskostenabrechnung(
+                dfake, vfake.rowid, jahr, beginn, ende);
+
+            stub.Should().BeOfType<Betriebskostenabrechnung>();
         }
 
         [Fact()]
