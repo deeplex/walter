cab801be8b5427e78c33d60adf716a462054d95b
klawr <mail@kailawrence.de>
2022-05-08 21:29:42 +0200

    Close #26

--- Deeplex.Saverwalter.Model/Migrations/20220508192646_UpdateToEFCore5.Designer.cs
+++ Deeplex.Saverwalter.Model/Migrations/20220508192646_UpdateToEFCore5.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Deeplex.Saverwalter.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Deeplex.Saverwalter.Model.Migrations
 {
     [DbContext(typeof(SaverwalterContext))]
-    partial class SaverwalterContextModelSnapshot : ModelSnapshot
+    [Migration("20220508192646_UpdateToEFCore5")]
+    partial class UpdateToEFCore5
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
