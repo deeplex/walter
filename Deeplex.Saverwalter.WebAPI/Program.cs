002eb09a2f84007991bb28074ae1b5c656acf69d
klawr <mail@kailawrence.de>
2023-02-25 17:33:27 +0100

    Add OTLP

--- Deeplex.Saverwalter.WebAPI/Program.cs
+++ Deeplex.Saverwalter.WebAPI/Program.cs
@@ -2,11 +2,19 @@ using Deeplex.Saverwalter.Model;
 using Deeplex.Saverwalter.Services;
 using Deeplex.Saverwalter.WebAPI.Services;
 using SimpleInjector;
+using OpenTelemetry.Exporter;
+using OpenTelemetry.Resources;
+using OpenTelemetry.Trace;
+using System.Diagnostics;
 
 namespace Deeplex.Saverwalter.WebAPI
 {
     public class Program
     {
+        public static string AppVersion = Environment.GetEnvironmentVariable("WalterVersion") ?? "v0.0.0";
+        public static string AppName = "Saverwalter";
+        private static string APMServer = Environment.GetEnvironmentVariable("APM-Server") ?? "http://192.168.178.61:8200"; // TODO change to localhost...
+
         private static Container Container { get; set; } = null!;
         public static SaverwalterContext ctx => Container.GetInstance<IWalterDbService>().ctx;
         public static TEntity LoadNavigations<TEntity>(TEntity entity)
@@ -26,6 +34,18 @@ namespace Deeplex.Saverwalter.WebAPI
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddOpenTelemetry()
+                .WithTracing(tracerProviderBuilder => tracerProviderBuilder
+                    .AddSource(AppName)
+                    .AddOtlpExporter(opt =>
+                    {
+                        opt.Endpoint = new Uri(APMServer);
+                        opt.Protocol = OtlpExportProtocol.Grpc;
+                    })
+                    .SetResourceBuilder(ResourceBuilder.CreateDefault().AddService(serviceName: AppName, serviceVersion: AppVersion))
+                    .AddHttpClientInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddSqlClientInstrumentation());
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
