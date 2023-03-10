3e553b9c33365fe6735ceb89f2713bc614bd945d
klawr <mail@kailawrence.de>
2023-03-10 18:44:06 +0100

    Revoke ObjectURL on closing modal
    This may be a memory leak

--- Deeplex.Saverwalter.WebAPI/svelte/src/components/elements/WalterPreview.svelte
+++ Deeplex.Saverwalter.WebAPI/svelte/src/components/elements/WalterPreview.svelte
@@ -1,7 +1,16 @@
 <script lang="ts">
 	import { WalterPreviewPdf } from '$WalterComponents';
 	import { download_file_blob } from '$WalterServices/s3';
-	import { ImageLoader, Modal, Tile } from 'carbon-components-svelte';
+	import {
+		Button,
+		ComposedModal,
+		ImageLoader,
+		Modal,
+		ModalBody,
+		ModalFooter,
+		ModalHeader,
+		Tile
+	} from 'carbon-components-svelte';
 
 	export let open: boolean = false;
 	export let blob: Blob | undefined = undefined;
@@ -19,37 +28,53 @@
 		}
 	}
 
+	function handleModalClose() {
+		URL.revokeObjectURL(objectURL);
+	}
+
 	function download() {
 		if (blob) {
 			download_file_blob(blob, name);
 		}
 	}
+
+	function remove() {}
+
+	let objectURL: string;
+	function createObjectURL(blob: Blob): string {
+		objectURL = URL.createObjectURL(blob);
+
+		return objectURL;
+	}
 </script>
 
-<Modal
+<ComposedModal
 	bind:open
-	bind:modalHeading={name}
-	primaryButtonText="Herunterladen"
-	secondaryButtonText="Abbrechen"
-	on:click:button--primary={download}
-	on:click:button--secondary={() => (open = false)}
 	on:open={handleModalOpen}
-	on:close
+	on:close={handleModalClose}
 	on:submit
 >
-	{#if blob}
-		{#if blob.type === 'image/png'}
-			<ImageLoader src={URL.createObjectURL(blob)} />
-		{:else if blob.type === 'text/plain'}
-			<Tile light>{text}</Tile>
-		{:else if blob.type === 'application/pdf'}
-			<div style="height:100vw">
-				<WalterPreviewPdf src={URL.createObjectURL(blob)} />
-			</div>
-		{:else}
-			<Tile light>
-				Kann für die Datei: {name} keine Vorschau anzeigen. Dateityp: {blob.type}.
-			</Tile>
+	<ModalHeader title={name} />
+	<ModalBody>
+		{#if blob}
+			{#if blob.type === 'image/png'}
+				<ImageLoader src={createObjectURL(blob)} />
+			{:else if blob.type === 'text/plain'}
+				<Tile light>{text}</Tile>
+			{:else if blob.type === 'application/pdf'}
+				<div style="height:100vw">
+					<WalterPreviewPdf src={createObjectURL(blob)} />
+				</div>
+			{:else}
+				<Tile light>
+					Kann für die Datei: {name} keine Vorschau anzeigen. Dateityp: {blob.type}.
+				</Tile>
+			{/if}
 		{/if}
-	{/if}
-</Modal>
+	</ModalBody>
+	<ModalFooter>
+		<Button kind="secondary" on:click={close}>Abbrechen</Button>
+		<Button kind="danger" on:click={remove}>Löschen</Button>
+		<Button kind="primary" on:click={download}>Herunterladen</Button>
+	</ModalFooter>
+</ComposedModal>
