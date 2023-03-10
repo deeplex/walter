ef1d05b7dcc7360ebfc7cf7ccfdfffc6e2259b10
klawr <mail@kailawrence.de>
2023-03-10 15:02:50 +0100

    Replace xml2json function with fast-xml-parser

--- Deeplex.Saverwalter.WebAPI/svelte/src/services/s3.ts
+++ Deeplex.Saverwalter.WebAPI/svelte/src/services/s3.ts
@@ -1,3 +1,5 @@
+import * as parser from 'fast-xml-parser';
+
 const baseURL = "http://192.168.178.61:9002/saverwalter";
 type fetchType = (input: RequestInfo | URL, init?: RequestInit | undefined) => Promise<Response>
 
@@ -29,6 +31,11 @@ export function download_file_blob(blob: Blob, fileName: string) {
     URL.revokeObjectURL(url);
 }
 
+type Content = {
+    Key: string;
+    LastModified: string
+}
+
 export function get_files_with_common_prefix(url: string, f: fetchType) {
     return f(`${baseURL}?prefix=${url}`, {
         method: 'GET',
@@ -36,63 +43,20 @@ export function get_files_with_common_prefix(url: string, f: fetchType) {
     })
         .then((e) => e.body?.getReader().read())
         .then((e) => {
-            const json: any = e ? xml2json(new TextDecoder().decode(e.value)) : {};
-            if (Array.isArray(json.Contents)) {
-                const a = json.Contents
-                    .map((e: any) => e.Key["#text"])
-                    .map((e: string) => e.split("/").pop());
-                return a;
-            }
-            else if (json.Contents?.Key) {
-                return [json.Contents?.Key["#text"].split("/").pop()];
-            }
-            else {
+            if (!e) {
                 return [];
             }
+            const result = new parser.XMLParser().parse(new TextDecoder().decode(e.value));
+            const Contents = result?.ListBucketResult?.Contents;
+            if (!Contents) {
+                return [];
+            }
+            else if (Array.isArray(Contents)) {
+                // Key is the full filepath, therefore split("/").pop() to get the filename
+                return Contents.map((e: Content) => e.Key.split("/").pop()) || [];
+            }
+            else {
+                return [Contents.Key];
+            }
         });
-}
-
-function xml2json(xmlString: string) {
-    const parser = new DOMParser();
-    const xml = parser.parseFromString(xmlString, 'text/xml');
-    const person = xmlToJson(xml.documentElement);
-
-    function xmlToJson(xml: Node): any {
-        let obj: any = {};
-        if (xml.nodeType === 1) {
-            const element = xml as Element;
-            if (element.attributes && element.attributes.length > 0) {
-                obj['@attributes'] = {};
-                for (let i = 0; i < element.attributes.length; i++) {
-                    const attribute = element.attributes.item(i);
-                    if (attribute) {
-                        obj['@attributes'][attribute.nodeName] = attribute.nodeValue;
-                    }
-                }
-            }
-        } else if (xml.nodeType === 3) {
-            obj = xml.nodeValue?.trim();
-        }
-        if (xml.hasChildNodes()) {
-            for (let i = 0; i < xml.childNodes.length; i++) {
-                const item = xml.childNodes.item(i);
-                if (item) {
-                    const nodeName = item.nodeName;
-                    if (typeof obj[nodeName] === 'undefined') {
-                        obj[nodeName] = xmlToJson(item);
-                    } else {
-                        if (typeof obj[nodeName].push === 'undefined') {
-                            const old = obj[nodeName];
-                            obj[nodeName] = [];
-                            obj[nodeName].push(old);
-                        }
-                        obj[nodeName].push(xmlToJson(item));
-                    }
-                }
-            }
-        }
-        return obj;
-    }
-
-    return person;
 }
