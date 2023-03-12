436ce19971435a9b1aeb469ea270e4d6b063e4e0
klawr <mail@kailawrence.de>
2023-03-12 10:40:19 +0100

    Add Einheit to WalterZaehlerstand

--- Deeplex.Saverwalter.WebAPI/svelte/src/components/details/WalterZaehlerstand.svelte
+++ Deeplex.Saverwalter.WebAPI/svelte/src/components/details/WalterZaehlerstand.svelte
@@ -6,6 +6,7 @@
 	} from '$WalterComponents';
 	import { Row } from 'carbon-components-svelte';
 	import type { WalterZaehlerstandEntry } from '$WalterTypes';
+	import WalterTextInput from '../elements/WalterTextInput.svelte';
 
 	export let a: Partial<WalterZaehlerstandEntry> = {};
 </script>
@@ -13,7 +14,7 @@
 <Row>
 	<WalterNumberInput bind:value={a.stand} label="Zählerstand" />
 	<WalterDatePicker bind:value={a.datum} labelText="Ablesedatum" />
-	<!-- TODO add einheit? -->
+	<WalterTextInput readonly bind:value={a.einheit} labelText="Einehit" />
 </Row>
 <Row>
 	<WalterTextArea labelText="Notiz" bind:value={a.notiz} />
