ef51f66c4a0fa938a28266737f44cae482868d50
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-04-21 18:13:13 +0200

    feat(model): Add a Postgres DbContext factory
    The DbContext factory can be used to inform the ef core design time
    tooling to correctly extract everything necessary for generating
    database migrations. See also [dbcontext-creation](https://learn.microsoft.com/en-us/ef/core/cli/dbcontext-creation?tabs=dotnet-core-cli#from-a-design-time-factory)

---
+++ Deeplex.Saverwalter.Model/SaverWalterContextFactory.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Deeplex.Saverwalter.Model
+{
+    public class SaverWalterContextFactory : IDesignTimeDbContextFactory<SaverwalterContext>
+    {
+        public SaverwalterContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SaverwalterContext>();
+            optionsBuilder.UseNpgsql(
+                 $@"Server=127.0.0.1
+                ;Database=saverdesign");
+
+            return new SaverwalterContext(optionsBuilder.Options);
+        }
+    }
+}
