b1f58eb9864beffcd8854cbe7ec4f25371607404
Henrik S. Gaßmann <henrik@gassmann.onl>
2023-04-21 19:23:08 +0200

    docs: Properly annotate the account REST endpoints

--- Deeplex.Saverwalter.WebAPI/Controllers/Utils/AccountController.cs
+++ Deeplex.Saverwalter.WebAPI/Controllers/Utils/AccountController.cs
@@ -28,11 +28,15 @@ namespace Deeplex.Saverwalter.WebAPI.Controllers.Utils
         }
         public class LoginResult
         {
-            public string? UserId { get; set; }
-            public string? Token { get; set; }
+            public string UserId { get; set; }
+            public string Token { get; set; }
         }
 
         [HttpPost("refresh-token")]
+        [Produces("text/plain")]
+        [ProducesErrorResponseType(typeof(void))] // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1752#issue-663991077
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<string>> RefreshToken()
         {
             var user = _httpContextAccessor.HttpContext?.User;
@@ -52,6 +56,10 @@ namespace Deeplex.Saverwalter.WebAPI.Controllers.Utils
 
         [HttpPost("sign-in")]
         [AllowAnonymous]
+        [Produces("application/json")]
+        [ProducesErrorResponseType(typeof(void))] // https://github.com/domaindrivendev/Swashbuckle.AspNetCore/issues/1752#issue-663991077
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
         public async Task<ActionResult<LoginResult>> SignIn([FromBody] LoginRequest loginRequest)
         {
             var result = await _userService.SignInAsync(loginRequest.Username, loginRequest.Password);
@@ -59,7 +67,7 @@ namespace Deeplex.Saverwalter.WebAPI.Controllers.Utils
             {
                 return new LoginResult
                 {
-                    Token = result.SessionToken,
+                    Token = result.SessionToken!,
                     UserId = result.Account!.Id.ToString("D", CultureInfo.InvariantCulture)
                 };
             }
