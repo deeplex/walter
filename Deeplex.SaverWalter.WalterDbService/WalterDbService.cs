147524dff6c48c73360241c1d68957799146f9fd
Kai <mail@kailawrence.de>
2023-05-02 19:56:56 +0000

    Remove deprecated project files

--- Deeplex.SaverWalter.WalterDbService/WalterDbService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Deeplex.Saverwalter.Model;
-namespace Deeplex.Saverwalter.WalterDbService
-{
-    public interface WalterDb
-    {
-        SaverwalterContext ctx { get; set; }
-        public void SaveWalter();
-    }
-}
