fe9e1f1ba3d90b550d1c8d7d09f33958918f3dcf
klawr <mail@kailawrence.de>
2023-06-18 14:58:51 +0200

    Remove steppers from Einheiten in Wohnung
    Weird formatting...

--- Deeplex.Saverwalter.WebAPI/svelte/src/components/details/WalterWohnung.svelte
+++ Deeplex.Saverwalter.WebAPI/svelte/src/components/details/WalterWohnung.svelte
@@ -25,11 +25,7 @@
     <WalterTextInput bind:value={entry.bezeichnung} labelText="Bezeichnung" />
     <WalterNumberInput bind:value={entry.wohnflaeche} label="Wohnfläche" />
     <WalterNumberInput bind:value={entry.nutzflaeche} label="Nutzfläche" />
-    <WalterNumberInput
-        hideSteppers={false}
-        bind:value={entry.einheiten}
-        label="Einheiten"
-    />
+    <WalterNumberInput bind:value={entry.einheiten} label="Einheiten" />
 </Row>
 <Row>
     <WalterTextArea bind:value={entry.notiz} labelText="Notiz" />
