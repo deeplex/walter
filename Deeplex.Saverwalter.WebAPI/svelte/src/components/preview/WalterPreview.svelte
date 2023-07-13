d0a42e96bcd7f9929348a0a9508a450832cf6ab7
klawr <mail@kailawrence.de>
2023-07-13 21:57:48 +0200

    Move FilePreviewCopy handling into tabs
    Remove modal in modal

--- Deeplex.Saverwalter.WebAPI/svelte/src/components/preview/WalterPreview.svelte
+++ Deeplex.Saverwalter.WebAPI/svelte/src/components/preview/WalterPreview.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-    import {
-        WalterPreviewImage,
-        WalterPreviewPdf,
-        WalterPreviewText,
-        WalterPreviewUnknown
-    } from '$walter/components';
     import type { WalterS3File } from '$walter/types';
     import {
         Button,
         ComposedModal,
         ModalBody,
         ModalFooter,
-        ModalHeader
+        ModalHeader,
+        Tab,
+        Tabs
     } from 'carbon-components-svelte';
-    import WalterPreviewCopyFile from '../preview/WalterPreviewCopyFile.svelte';
-    import { download, remove } from '../preview/WalterPreview';
-    import type { WalterS3FileWrapper } from '$walter/lib';
+    import WalterPreviewCopyFile from './WalterPreviewCopyFile.svelte';
+    import { download, remove } from './WalterPreview';
+    import type {
+        WalterS3FileWrapper,
+        WalterSelectionEntry
+    } from '$walter/lib';
+    import WalterPreviewType from './WalterPreviewDataTypeSelector.svelte';
+    import { Download } from 'carbon-icons-svelte';
+    import {
+        copyImpl,
+        moveImpl,
+        type WalterPreviewCopyTable
+    } from './WalterPreviewCopyFile';
 
     export let open = false;
     export let file: WalterS3File;
@@ -25,39 +31,117 @@
         open = false;
     }
 
-    let copying = false;
-    function copy() {
-        copying = true;
-    }
-
-    function click_remove(e: MouseEvent): void {
-        remove(file, fileWrapper);
-    }
-
     function click_download(e: MouseEvent): void {
         download(file);
+    }
+    let selectedTab = 0;
+
+    let step = 0;
+    let selectedTable: WalterPreviewCopyTable | undefined = undefined;
+    let selectedEntry: WalterSelectionEntry | undefined = undefined;
+
+    async function copy() {
+        const copied = await copyImpl(
+            file,
+            fileWrapper.fetchImpl,
+            selectedTable,
+            selectedEntry
+        );
+
+        if (copied && selectedTable && selectedEntry) {
+            open = false;
+
+            fileWrapper.addFile(
+                file,
+                `${selectedTable.key}/${selectedEntry.id}`
+            );
+        }
+    }
+
+    async function move() {
+        const moved = await moveImpl(
+            file,
+            fileWrapper.fetchImpl,
+            selectedTable,
+            selectedEntry
+        );
+
+        if (moved && selectedTable && selectedEntry) {
+            open = false;
+            fileWrapper.addFile(
+                file,
+                `${selectedTable.key}/${selectedEntry.id}`
+            );
+            fileWrapper.removeFile(file);
+        }
+    }
+
+    function submit() {
+        switch (selectedTab) {
+            case TabSelector.Copy:
+                copy();
+                break;
+            case TabSelector.Move:
+                move();
+                break;
+            case TabSelector.Delete:
+                remove(file, fileWrapper);
+                break;
+            default:
+                break;
+        }
+    }
+
+    enum TabSelector {
+        Preview,
+        Copy,
+        Move,
+        Delete
     }
 </script>
 
 <ComposedModal size="lg" bind:open on:submit>
-    <ModalHeader bind:title={file.FileName} />
+    <ModalHeader bind:title={file.FileName}>
+        <div style="display: flex; flex-direction: row">
+            <Tabs bind:selected={selectedTab}>
+                <Tab label="Vorschau" />
+                <Tab label="Kopieren" />
+                <Tab label="Verschieben" />
+                <Tab label="Löschen" />
+            </Tabs>
+            <Button
+                on:click={click_download}
+                style="top: -3em"
+                kind="tertiary"
+                iconDescription="Herunterladen"
+                icon={Download}
+            />
+        </div>
+    </ModalHeader>
     <ModalBody>
-        {#if file.Type?.includes('image/')}
-            <WalterPreviewImage {file} />
-        {:else if file.Type === 'text/plain' || file.Type === 'application/json'}
-            <WalterPreviewText {file} />
-        {:else if file.Type === 'application/pdf'}
-            <WalterPreviewPdf {file} />
-        {:else}
-            <WalterPreviewUnknown {file} />
+        {#if selectedTab === TabSelector.Preview}
+            <WalterPreviewType {file} />
+        {:else if selectedTab === TabSelector.Copy || selectedTab === TabSelector.Move}
+            <WalterPreviewCopyFile
+                bind:selectedTable
+                bind:selectedEntry
+                bind:step
+                {fileWrapper}
+            />
+        {:else if selectedTab === TabSelector.Delete}
+            <p>Datei {file.FileName} unwiderruflich löschen</p>
         {/if}
     </ModalBody>
-    <ModalFooter>
-        <Button kind="secondary" on:click={close}>Abbrechen</Button>
-        <Button kind="tertiary" on:click={copy}>Kopieren / Verschieben</Button>
-        <Button kind="danger" on:click={click_remove}>Löschen</Button>
-        <Button kind="primary" on:click={click_download}>Herunterladen</Button>
-    </ModalFooter>
+    {#if selectedTab !== TabSelector.Preview}
+        <ModalFooter>
+            <Button kind="secondary" on:click={close}>Abbrechen</Button>
+            <Button
+                disabled={(selectedTab === TabSelector.Copy ||
+                    selectedTab === TabSelector.Move) &&
+                    step < 3}
+                kind={selectedTab === TabSelector.Delete ? 'danger' : 'primary'}
+                on:click={submit}>Bestätigen</Button
+            >
+        </ModalFooter>
+    {/if}
 </ComposedModal>
-
-<WalterPreviewCopyFile bind:fileWrapper bind:open={copying} {file} />
