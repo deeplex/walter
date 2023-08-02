b2e09e6a416f8b809bd7ce5bc6c98c4de4382443
klawr <mail@kailawrence.de>
2023-08-03 01:20:05 +0200

    Set extendedNav to true per default
    This is a trial phase and may be changed in the future

--- Deeplex.Saverwalter.WebAPI/svelte/src/components/preview/WalterPreview.svelte
+++ Deeplex.Saverwalter.WebAPI/svelte/src/components/preview/WalterPreview.svelte
@@ -165,7 +165,7 @@
             />
         </div>
     </ModalHeader>
-    <ModalBody  style="min-height: 40em;">
+    <ModalBody style="min-height: 40em;">
         {#if selectedTab === TabSelector.Preview}
             <WalterPreviewType bind:file />
         {:else if selectedTab === TabSelector.Copy || selectedTab === TabSelector.Move}
