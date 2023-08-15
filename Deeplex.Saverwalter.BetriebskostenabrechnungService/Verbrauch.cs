8592004ffd5b87cd72a1ceca702732107d823d45
klawr <mail@kailawrence.de>
2023-08-15 21:13:59 +0200

    Implement Zaehlerwechsel and update Notes

--- Deeplex.Saverwalter.BetriebskostenabrechnungService/Verbrauch.cs
+++ Deeplex.Saverwalter.BetriebskostenabrechnungService/Verbrauch.cs
@@ -5,26 +5,43 @@ namespace Deeplex.Saverwalter.BetriebskostenabrechnungService
     public sealed class Verbrauch
     {
         public Zaehler Zaehler;
+        public DateOnly Anfangsdatum;
+        public DateOnly Enddatum;
         public double Delta;
 
-        public Verbrauch(Zaehler zaehler, Zeitraum zeitraum, List<Note> notes)
+        public Verbrauch(Zaehler zaehler, DateOnly beginn, DateOnly ende, List<Note> notes)
         {
             Zaehler = zaehler;
-            var endstand = GetZaehlerEndStand(zaehler, zeitraum.Nutzungsende);
-            var anfangsstand = GetZaehlerAnfangsStand(zaehler, zeitraum.Nutzungsbeginn);
+            var endstand = GetZaehlerEndStand(zaehler, ende);
+            var anfangsstand = GetZaehlerAnfangsStand(zaehler, beginn);
             if (anfangsstand == null)
             {
-                notes.Add(new Note($"Keinen Anfangsstand für Zähler {zaehler.Kennnummer} ({zaehler.Typ}) gefunden.", Severity.Error));
+                notes.Add(new Note($"Keinen gültigen Anfangsstand für Zähler {zaehler.Kennnummer} ({zaehler.Typ}) " +
+                    $"innerhalb des Zeitraums ({beginn.ToString("dd.MM.yyyy")} - {ende.ToString("dd.MM.yyyy")}) gefunden.",
+                    Severity.Error));
             }
+            else
+            {
+                Anfangsdatum = anfangsstand.Datum;
+            }
+
             if (endstand == null)
             {
-                notes.Add(new Note($"Keinen Endstand für Zähler {zaehler.Kennnummer} ({zaehler.Typ}) gefunden.", Severity.Error));
+                notes.Add(new Note($"Keinen gültigen Endstand für Zähler {zaehler.Kennnummer} ({zaehler.Typ})" +
+                    $"innerhalb des Zeitraums ({beginn.ToString("dd.MM.yyyy")} - {ende.ToString("dd.MM.yyyy")}) gefunden.",
+                    Severity.Error));
+            }
+            else
+            {
+                Enddatum = endstand.Datum;
             }
 
             if (anfangsstand != null && endstand != null)
             {
                 Delta = endstand.Stand - anfangsstand.Stand;
             }
+
+
             else if (anfangsstand == null && endstand?.Stand == 0)
             {
                 Delta = 0;
@@ -35,18 +52,19 @@ namespace Deeplex.Saverwalter.BetriebskostenabrechnungService
             }
         }
 
-        private static Zaehlerstand? GetZaehlerEndStand(Zaehler zaehler, DateOnly nutzungsende)
+        private static Zaehlerstand? GetZaehlerEndStand(Zaehler zaehler, DateOnly ende)
         {
             return zaehler.Staende
-                    .OrderBy(s => s.Datum)
-                    .LastOrDefault(l => l.Datum <= nutzungsende && (nutzungsende.DayNumber - l.Datum.DayNumber) < 30);
+                    .OrderBy(zaehlerstand => zaehlerstand.Datum)
+                    .LastOrDefault(zaehlerstand => zaehlerstand.Datum <= ende);
         }
 
-        private static Zaehlerstand? GetZaehlerAnfangsStand(Zaehler zaehler, DateOnly nutzungsbeginn)
+        private static Zaehlerstand? GetZaehlerAnfangsStand(Zaehler zaehler, DateOnly beginn)
         {
+            var earliest = beginn.AddDays(-14).DayNumber;
             return zaehler.Staende
-                .OrderBy(s => s.Datum)
-                .FirstOrDefault(l => Math.Abs(l.Datum.DayNumber - nutzungsbeginn.AddDays(-1).DayNumber) < 30);
+                .OrderBy(zaehlerstand => zaehlerstand.Datum)
+                .FirstOrDefault(zaehlerstand => earliest <= zaehlerstand.Datum.DayNumber);
         }
 
 
--- Deeplex.Saverwalter.BetriebskostenabrechnungService/Verbrauchanteil.cs
+++ Deeplex.Saverwalter.BetriebskostenabrechnungService/Verbrauchanteil.cs
@@ -31,15 +31,25 @@ namespace Deeplex.Saverwalter.BetriebskostenabrechnungService
 
                 foreach (var zaehler in zaehlergroup)
                 {
-                    var verbrauch = new Verbrauch(zaehler, zeitraum, notes);
+                    var verbrauch = new Verbrauch(zaehler, zeitraum.Abrechnungsbeginn, zeitraum.Abrechnungsende, notes);
                     AlleZaehler[unit].Add(verbrauch);
                     AlleVerbrauch[unit] += verbrauch.Delta;
 
                     if (zaehler.Wohnung == wohnung)
                     {
-                        DieseZaehler[unit].Add(verbrauch);
+                        var verbrauchMieter = new Verbrauch(zaehler, zeitraum.Nutzungsbeginn, zeitraum.Nutzungsende, notes);
+                        DieseZaehler[unit].Add(verbrauchMieter);
                         DieseVerbrauch[unit] += verbrauch.Delta;
                     }
+                }
+
+                foreach (var entry in AlleZaehler)
+                {
+                    checkIfZaehlerstaendeValid(entry.Value, zeitraum.Abrechnungsbeginn, zeitraum.Abrechnungsende, notes);
+                }
+                foreach (var entry in DieseZaehler)
+                {
+                    checkIfZaehlerstaendeValid(entry.Value, zeitraum.Nutzungsbeginn, zeitraum.Nutzungsende, notes);
                 }
             }
 
@@ -52,6 +62,54 @@ namespace Deeplex.Saverwalter.BetriebskostenabrechnungService
                     Anteil[zaehlerTyp.Key] = 0;
                 }
             }
+        }
+
+        private static void checkIfZaehlerstaendeValid(
+            List<Verbrauch> verbraeuche,
+            DateOnly beginn,
+            DateOnly ende,
+            List<Note> notes)
+        {
+            foreach (var verbrauch in verbraeuche)
+            {
+                if (verbrauch.Enddatum.DayNumber < ende.DayNumber - 14)
+                {
+                    var ersatz = verbraeuche.SingleOrDefault(other_verbrauch =>
+                        other_verbrauch.Anfangsdatum == verbrauch.Enddatum &&
+                        other_verbrauch.Zaehler.Staende.OrderBy(stand => stand.Datum).First().Datum == other_verbrauch.Anfangsdatum &&
+                        verbrauch.Zaehler.Wohnung == other_verbrauch.Zaehler.Wohnung);
+                    if (ersatz != null)
+                    {
+                        notes.Add(new Note($"Zählerwechsel erkannt am {verbrauch.Enddatum} von Zähler " +
+                            $"{verbrauch.Zaehler.Kennnummer} auf {ersatz.Zaehler.Kennnummer} ({ersatz.Zaehler.Typ})",
+                            Severity.Info));
+                    }
+                    else
+                    {
+                        notes.Add(new Note($"Kein Endstand für Zähler {verbrauch.Zaehler.Kennnummer} ({verbrauch.Zaehler.Typ}) gefunden. " +
+                            $"Letzter gültiger Zählerstand ist vom {verbrauch.Enddatum}, was nicht innerhalb von 14 Tagen vor Nutzungsende ist.",
+                            Severity.Error));
+                    }
+                }
+
+                if (verbrauch.Anfangsdatum > beginn)
+                {
+                    var vorgaenger = verbraeuche.SingleOrDefault(other_verbrauch =>
+                        other_verbrauch.Enddatum == verbrauch.Anfangsdatum &&
+                        verbrauch.Zaehler.Wohnung == other_verbrauch.Zaehler.Wohnung);
+                    if (vorgaenger != null)
+                    {
+                        // Wurde bereits beim abgelösten Teil klar gemacht.
+                    }
+                    else
+                    {
+                        notes.Add(new Note($"Kein Anfangsstand für Zähler {verbrauch.Zaehler.Kennnummer} ({verbrauch.Zaehler.Typ}) gefunden. " +
+                            $"Erster gültiger Zählerstand ist vom {verbrauch.Anfangsdatum}, was nach Nutzungsbeginn ist.",
+                            Severity.Error));
+                    }
+                }
+            }
+
         }
 
         public static List<VerbrauchAnteil> GetVerbrauchAnteile(
