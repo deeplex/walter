d8fc9d7eae2ccf9308d554a1aa27c6faf7df76fb
klawr <mail@kailawrence.de>
2023-11-09 16:15:01 +0100

    Add BetriebskostenrechnungController.Tests

---
+++ Deeplex.Saverwalter.WebAPI.Tests/Controllers/BetriebskostenrechnungController.Tests.cs
@@ -0,0 +1,117 @@
+using Xunit;
+using Deeplex.Saverwalter.ModelTests;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using FakeItEasy;
+using Deeplex.Saverwalter.WebAPI.Controllers;
+using Deeplex.Saverwalter.WebAPI.Services.ControllerService;
+using static Deeplex.Saverwalter.WebAPI.Controllers.BetriebskostenrechnungController;
+using Deeplex.Saverwalter.Model;
+
+namespace Deeplex.Saverwalter.WebAPI.Tests
+{
+    public class BetriebskostenrechnungControllerTests
+    {
+        [Fact]
+        public void Get()
+        {
+            var ctx = TestUtils.GetContext();
+            var logger = A.Fake<ILogger<BetriebskostenrechnungController>>();
+            var dbService = new BetriebskostenrechnungDbService(ctx);
+            var controller = new BetriebskostenrechnungController(logger, dbService);
+
+            var result = controller.Get();
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void Post()
+        {
+            var ctx = TestUtils.GetContext();
+            var logger = A.Fake<ILogger<BetriebskostenrechnungController>>();
+            var dbService = new BetriebskostenrechnungDbService(ctx);
+            var controller = new BetriebskostenrechnungController(logger, dbService);
+
+            var umlage = new Umlage(Betriebskostentyp.Dachrinnenreinigung, Umlageschluessel.NachWohnflaeche);
+            ctx.Umlagen.Add(umlage);
+            ctx.SaveChanges();
+            var entity = new Betriebskostenrechnung(1000, new DateOnly(2021, 1, 1), 2021)
+            {
+                Umlage = umlage
+            };
+            var entry = new BetriebskostenrechnungEntry(entity, ctx);
+
+            var result = controller.Post(entry);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void GetId()
+        {
+            var ctx = TestUtils.GetContext();
+            var vertrag = TestUtils.GetVertragForAbrechnung(ctx);
+            var logger = A.Fake<ILogger<BetriebskostenrechnungController>>();
+            var dbService = new BetriebskostenrechnungDbService(ctx);
+            var controller = new BetriebskostenrechnungController(logger, dbService);
+
+            var entity = vertrag.Wohnung.Umlagen.First().Betriebskostenrechnungen.First();
+            if (entity == null)
+            {
+                throw new NullReferenceException("Betriebskostenrechnung is null");
+            }
+
+            var result = controller.Get(entity.BetriebskostenrechnungId);
+
+            result.Should().BeOfType<OkObjectResult>();
+        }
+
+        [Fact]
+        public void Put()
+        {
+            var ctx = TestUtils.GetContext();
+            var vertrag = TestUtils.GetVertragForAbrechnung(ctx);
+            var logger = A.Fake<ILogger<BetriebskostenrechnungController>>();
+            var dbService = new BetriebskostenrechnungDbService(ctx);
+            var controller = new BetriebskostenrechnungController(logger, dbService);
+
+            var entity = vertrag.Wohnung.Umlagen.First().Betriebskostenrechnungen.First();
+            if (entity == null)
+            {
+                throw new NullReferenceException("Betriebskostenrechnung is null");
+            }
+            var entry = new BetriebskostenrechnungEntry(entity, ctx);
+            entry.Betrag = 2000;
+
+            var result = controller.Put(entity.BetriebskostenrechnungId, entry);
+
+            result.Should().BeOfType<OkObjectResult>();
+            entity.Betrag.Should().Be(2000);
+        }
+
+        [Fact]
+        public void Delete()
+        {
+            var ctx = TestUtils.GetContext();
+            var vertrag = TestUtils.GetVertragForAbrechnung(ctx);
+            var logger = A.Fake<ILogger<BetriebskostenrechnungController>>();
+            var dbService = new BetriebskostenrechnungDbService(ctx);
+            var controller = new BetriebskostenrechnungController(logger, dbService);
+
+            var entity = vertrag.Wohnung.Umlagen.First().Betriebskostenrechnungen.First();
+            if (entity == null)
+            {
+                throw new NullReferenceException("Entity is null");
+            }
+            var id = entity.BetriebskostenrechnungId;
+
+            var result = controller.Delete(id);
+
+            result.Should().BeOfType<OkResult>();
+            ctx.Betriebskostenrechnungen.Find(id).Should().BeNull();
+
+        }
+    }
+}
