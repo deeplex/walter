e64f3dfa88d170dbd6976044a5b37bec2af0f07c
klawr <mail@kailawrence.de>
2023-11-18 23:48:26 +0100

    Add proper Migration for Umlagetypen
    Co-authored-by: Henrik S. Gaßmann <henrik@gassmann.onl>

--- Deeplex.Saverwalter.Model/Migrations/Npgsql/20231118212401_AddTablesUmlagetypenAndHKVO.Designer.cs
+++ Deeplex.Saverwalter.Model/Migrations/Npgsql/20231118212401_AddTablesUmlagetypenAndHKVO.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Deeplex.Saverwalter.Model;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Deeplex.Saverwalter.Model.Migrations.Npgsql
 {
     [DbContext(typeof(SaverwalterContext))]
-    partial class SaverwalterContextModelSnapshot : ModelSnapshot
+    [Migration("20231118212401_AddTablesUmlagetypenAndHKVO")]
+    partial class AddTablesUmlagetypenAndHKVO
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
