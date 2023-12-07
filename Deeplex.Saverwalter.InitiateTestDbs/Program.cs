18a0cf5f18bae1c0de75c0592d8b9b604658d285
klawr <mail@kailawrence.de>
2023-12-07 13:05:00 +0100

    fix: Remove header from .editorconfig

--- Deeplex.Saverwalter.InitiateTestDbs/Program.cs
+++ Deeplex.Saverwalter.InitiateTestDbs/Program.cs
@@ -1,8 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
-
-using Deeplex.Saverwalter.InitiateTestDbs.Templates;
+﻿using Deeplex.Saverwalter.InitiateTestDbs.Templates;
 
 namespace Deeplex.Saverwalter.InitiateTestDbs
 {
