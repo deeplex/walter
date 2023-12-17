7ac4e920038b5123b28ee08333aac9e99727e09b
klawr <mail@kailawrence.de>
2023-12-17 11:25:58 +0100

    feat(WebAPI): Move most of the FileController Logic into Utils class

--- Deeplex.Saverwalter.WebAPI/Controllers/Utils/FileControllerBase.cs
+++ Deeplex.Saverwalter.WebAPI/Controllers/Utils/FileControllerBase.cs
@@ -1,8 +1,7 @@
-﻿using System.Net.Http.Headers;
-using Deeplex.Saverwalter.WebAPI.Services;
+﻿using Deeplex.Saverwalter.WebAPI.Services;
+using Deeplex.Saverwalter.WebAPI.Utils;
 using Microsoft.AspNetCore.Authorization.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.StaticFiles;
 
 namespace Deeplex.Saverwalter.WebAPI.Controllers
 {
@@ -18,142 +17,42 @@ namespace Deeplex.Saverwalter.WebAPI.Controllers
             _httpClient = httpClient;
         }
 
-        private static StreamContent FillContent(string path, HttpRequest request)
-        {
-            var content = new StreamContent(request.Body);
-
-            var contentLength = request.ContentLength ?? -1;
-            if (contentLength >= 0)
-            {
-                content.Headers.ContentLength = contentLength;
-            }
-
-            new FileExtensionContentTypeProvider()
-                .TryGetContentType(Path.GetExtension(path), out var contentType);
-            if (contentType != null)
-            {
-                content.Headers.ContentType = MediaTypeHeaderValue.Parse(contentType);
-            }
-
-            return content;
-        }
-
-        private async static Task<HttpResponseMessage> SaveToTrash(string path, HttpRequest request, HttpClient httpClient)
-        {
-            HttpContent content = null!;
-            // Put trash before the filename
-            var splits = path.Split('/');
-            var trashPath = $"{string.Join('/', splits.Reverse().Skip(1).Reverse())}/trash/{splits.Last()}";
-            var trashRequest = new HttpRequestMessage(new HttpMethod(HttpMethod.Put.Method), trashPath) { Content = content };
-            trashRequest.Content = FillContent(path, request);
-            var trashResponse = await httpClient.SendAsync(trashRequest, CancellationToken.None);
-
-            return trashResponse;
-        }
-
-        private async Task<IActionResult> RedirectToFileServer(string path)
-        {
-            var requestMethod = HttpContext.Request.Method;
-
-            HttpContent content = null!;
-            var request = new HttpRequestMessage(new HttpMethod(requestMethod), path) { Content = content };
-
-            if (requestMethod == HttpMethod.Put.Method)
-            {
-                request.Content = FillContent(path, Request);
-            }
-            else if (requestMethod == HttpMethod.Delete.Method)
-            {
-                var trashResponse = await SaveToTrash(path, Request, _httpClient);
-
-                if (!trashResponse.IsSuccessStatusCode)
-                {
-                    return StatusCode((int)trashResponse.StatusCode);
-                }
-            }
-
-            var response = await _httpClient.SendAsync(request, CancellationToken.None);
-            var responseContent = await response.Content.ReadAsByteArrayAsync();
-            var responseType = response.Content.Headers.ContentType?.MediaType ?? "plain/text";
-
-            return File(responseContent, responseType);
-        }
-
         protected abstract WalterDbServiceBase<T, U> DbService { get; }
 
-        private ActionResult<string> GetS3Path(string? filename = null)
-        {
-            if (_baseUrl is null)
-            {
-                return StatusCode(StatusCodes.Status503ServiceUnavailable);
-            }
-
-            var path = ControllerContext.HttpContext.Request.Path.Value;
-
-            if (string.IsNullOrEmpty(path))
-            {
-                return new BadRequestResult();
-            }
-
-            var splits = path.Split("/");
-
-            if (splits.Length < 4)
-            {
-                return new BadRequestResult();
-            }
-
-            string resourceUrl;
-
-            if (filename is string name)
-            {
-                if (splits[5] == "trash")
-                {
-                    resourceUrl = _baseUrl + "/" + splits[2] + "/" + splits[3] + "/" + splits[5] + "/" + name;
-                }
-                else
-                {
-                    resourceUrl = _baseUrl + "/" + splits[2] + "/" + splits[3] + "/" + name;
-                }
-            }
-            else
-            {
-                resourceUrl = _baseUrl + "?prefix=" + splits[2] + "/" + splits[3];
-            }
-
-            return resourceUrl;
-        }
-
-        private async Task<IActionResult> ProcessFileRequest(int id, OperationAuthorizationRequirement operation, string? filename = null)
+        private async Task<ActionResult?> Authorize(int id, OperationAuthorizationRequirement operation)
         {
             var entityResult = await DbService.GetEntity(User, id, operation);
-            if (entityResult is ActionResult entityActionResult)
-            {
-                return entityActionResult;
-            }
-
-            var s3Path = GetS3Path(filename);
-            if (s3Path.Result is ActionResult s3PathActionResult)
-            {
-                return s3PathActionResult;
-            }
-            else if (s3Path.Value is string fullPath)
-            {
-                return await RedirectToFileServer(fullPath);
-            }
-            else
-            {
-                return new BadRequestResult();
-            }
+            return entityResult.Result;
         }
 
         [HttpGet("{id}/files")]
-        public async Task<IActionResult> GetFiles(int id) => await ProcessFileRequest(id, Operations.Read);
-
+        public async Task<IActionResult> GetFiles(int id)
+        {
+            if (await Authorize(id, Operations.Read) is ActionResult authResult)
+            {
+                return authResult;
+            }
+            return await FileHandling.ProcessFileRequest(_baseUrl, Request, _httpClient);
+        }
         [HttpGet("{id}/files/{filename}")]
-        public async Task<IActionResult> ReadFile(int id, string filename) => await ProcessFileRequest(id, Operations.Read, filename);
+        public async Task<IActionResult> ReadFile(int id, string filename)
+        {
+            if (await Authorize(id, Operations.Read) is ActionResult authResult)
+            {
+                return authResult;
+            }
+            return await FileHandling.ProcessFileRequest(_baseUrl, Request, _httpClient, filename);
+        }
 
         [HttpDelete("{id}/files/{filename}")]
         [HttpPut("{id}/files/{filename}")]
-        public async Task<IActionResult> WriteFile(int id, string filename) => await ProcessFileRequest(id, Operations.Update, filename);
+        public async Task<IActionResult> WriteFile(int id, string filename)
+        {
+            if (await Authorize(id, Operations.Read) is ActionResult authResult)
+            {
+                return authResult;
+            }
+            return await FileHandling.ProcessFileRequest(_baseUrl, Request, _httpClient, filename);
+        }
     }
 }
---
+++ Deeplex.Saverwalter.WebAPI/Utils/FileHandling.cs
@@ -0,0 +1,126 @@
+﻿using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+
+namespace Deeplex.Saverwalter.WebAPI.Utils
+{
+    public static class FileHandling
+    {
+        private static StreamContent FillContent(string path, HttpRequest request)
+        {
+            var content = new StreamContent(request.Body);
+
+            var contentLength = request.ContentLength ?? -1;
+            if (contentLength >= 0)
+            {
+                content.Headers.ContentLength = contentLength;
+            }
+
+            new FileExtensionContentTypeProvider()
+                .TryGetContentType(Path.GetExtension(path), out var contentType);
+            if (contentType != null)
+            {
+                content.Headers.ContentType = MediaTypeHeaderValue.Parse(contentType);
+            }
+
+            return content;
+        }
+
+        private async static Task<HttpResponseMessage> SaveToTrash(string path, HttpRequest request, HttpClient httpClient)
+        {
+            HttpContent content = null!;
+            // Put trash before the filename
+            var splits = path.Split('/');
+            var trashPath = $"{string.Join('/', splits.Reverse().Skip(1).Reverse())}/trash/{splits.Last()}";
+            var trashRequest = new HttpRequestMessage(new HttpMethod(HttpMethod.Put.Method), trashPath) { Content = content };
+            trashRequest.Content = FillContent(path, request);
+            var trashResponse = await httpClient.SendAsync(trashRequest, CancellationToken.None);
+
+            return trashResponse;
+        }
+
+        private static ActionResult<string> GetS3Path(string? baseUrl, string? requestPath, string? filename = null)
+        {
+            if (baseUrl is null)
+            {
+                return new StatusCodeResult(StatusCodes.Status503ServiceUnavailable);
+            }
+
+            if (string.IsNullOrEmpty(requestPath))
+            {
+                return new BadRequestResult();
+            }
+
+            var splits = requestPath.Split("/");
+
+            if (splits.Length < 4)
+            {
+                return new BadRequestResult();
+            }
+
+            string resourceUrl;
+
+            if (filename is string name)
+            {
+                if (splits[5] == "trash")
+                {
+                    resourceUrl = baseUrl + "/" + splits[2] + "/" + splits[3] + "/" + splits[5] + "/" + name;
+                }
+                else
+                {
+                    resourceUrl = baseUrl + "/" + splits[2] + "/" + splits[3] + "/" + name;
+                }
+            }
+            else
+            {
+                resourceUrl = baseUrl + "?prefix=" + splits[2] + "/" + splits[3];
+            }
+
+            return resourceUrl;
+        }
+
+
+        private static async Task<IActionResult> RedirectToFileServer(HttpRequest request, HttpClient client, string path)
+        {
+            HttpContent content = null!;
+            var requestMessage = new HttpRequestMessage(new HttpMethod(request.Method), path) { Content = content };
+
+            if (request.Method == HttpMethod.Put.Method)
+            {
+                requestMessage.Content = FillContent(path, request);
+            }
+            else if (request.Method == HttpMethod.Delete.Method)
+            {
+                var trashResponse = await SaveToTrash(path, request, client);
+
+                if (!trashResponse.IsSuccessStatusCode)
+                {
+                    return new StatusCodeResult((int)trashResponse.StatusCode);
+                }
+            }
+
+            var response = await client.SendAsync(requestMessage, CancellationToken.None);
+            var responseContent = await response.Content.ReadAsByteArrayAsync();
+            var responseType = response.Content.Headers.ContentType?.MediaType ?? "plain/text";
+
+            return new FileContentResult(responseContent, responseType);
+        }
+
+        public static async Task<IActionResult> ProcessFileRequest(string? baseUrl, HttpRequest request, HttpClient client, string? filename = null)
+        {
+            var s3Path = GetS3Path(baseUrl, request.Path.Value, filename);
+            if (s3Path.Result is ActionResult s3PathActionResult)
+            {
+                return s3PathActionResult;
+            }
+            else if (s3Path.Value is string fullPath)
+            {
+                return await RedirectToFileServer(request, client, fullPath);
+            }
+            else
+            {
+                return new BadRequestResult();
+            }
+        }
+    }
+}
